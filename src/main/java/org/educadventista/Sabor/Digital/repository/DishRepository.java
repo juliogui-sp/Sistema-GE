d4ded463eee47a5af3e2dff78d5689f9a0121263
juliogui-sp <julio_guilherme@educadventista.org>
2024-12-05 12:57:04 -0300

    Master

--- src/main/java/org/educadventista/Sabor/Digital/repository/DishRepository.java
+++
@@ -1,7 +0,0 @@
-package org.educadventista.Sabor.Digital.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.educadventista.Sabor.Digital.model.Dish;
-
-public interface DishRepository extends JpaRepository<Dish, Long> {
-}
--- src/main/java/org/educadventista/Sabor/Digital/repository/OrderStatusRepository.java
+++
@@ -1,9 +0,0 @@
-package org.educadventista.Sabor.Digital.repository;
-
-import org.educadventista.Sabor.Digital.model.OrderStatus;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface OrderStatusRepository extends JpaRepository<OrderStatus, Long> {
-}
